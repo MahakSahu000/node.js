de75ef1861afc6bcffea65e9872bf49634a4f9b9
Mahak Sahu <mahak.sahu_cs23@gla.ac.in>
2026-01-22 23:25:07 +0530

    commit

--- file.js
+++ file.js
@@ -1,4 +1,20 @@
 const fs = require('fs');
+const os = require("os");
+
+console.log(os.cpus().length);
+console.log("1");
+// non-blocking / asynchronous
+const result = fs.readFile("contacts.txt","utf-8" , (err,result)=>{
+  console.log(result);
+});
+
+console.log("2");
+
+// default thread pool size = 4
+//max? - 8core cpu = 8 thread pool
+
+
+
 // fs.writeFileSync('./test.txt','hello world');
 
 // fs.writeFile('./test.txt','hello world Async', (err) =>{} );
@@ -15,9 +31,9 @@ const fs = require('fs');
 //   }
 // });
 
-fs.appendFileSync('./test.txt', `${Date.now()} hey there\n`);
+// fs.appendFileSync('./test.txt', `${Date.now()} hey there\n`);
 
-// fs.cpSync('./test.txt','./copy.txt');
-// fs.unlinkSync('./copy.txt');
-console.log(fs.statSync('./test.txt'));
-fs.mkdirSync("my-docs/a/b" , { recursive : true});
+// // fs.cpSync('./test.txt','./copy.txt');
+// // fs.unlinkSync('./copy.txt');
+// console.log(fs.statSync('./test.txt'));
+// fs.mkdirSync("my-docs/a/b" , { recursive : true});
