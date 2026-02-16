d17af77091c386aba2d9f147d28f0a5d13eefde8
Mahak Sahu <mahak.sahu_cs23@gla.ac.in>
2026-02-16 14:59:36 +0530

    16-02

--- secG/index.js
+++ secG/index.js
@@ -1,5 +1,35 @@
 // console.log("heeloo world.....");
-import add from './fun.js';
-console.log(add(5,10));
+// import add from './fun.js';
+// console.log(add(5,10));
+
+// console.log("start");
+// setTimeout(() =>{
+//   console.log("inside setTimeout");
+  
+// })
+setTimeout(() => {
+    console.log("hi")
+},1000);
+
+// promises
+
+const nayapromise =  new Promise((resolve , reject) => {
+  let deliver = true;
+  if(deliver){
+    resolve("Promises has been resolved successfully , your order is delivered");
+  }
+  else{
+    reject("Promises has been rejected , your order is not delivered");
+  }
+});
+
+nayapromise.then(() =>{
+  console.log("This is in the block");
+}).catch(() =>{
+  console.log("This is in the catch block");
+});
+
+
+
 
 
